787259267713e6d6fd3ea63a943fe6ac855e34b3
Robin Barker (EBU) <robin@ebu.co.uk>
2022-01-18 00:42:22 +0000

    options object

--- lib/Bridge/Flower.pm
+++ lib/Bridge/Flower.pm
@@ -2,20 +2,38 @@ package Bridge::Flower;
 
 use strict;
 use warnings;
-use parent qw( Exporter);
-
-our @EXPORT = qw( main );
-our $VERSION = '0.99';
 
 use Pod::Usage;
 use Getopt::Long(qw(:config posix_default no_ignore_case));
 use List::Util qw(sum);
 
+our $VERSION = '0.99';
+our $gcd = eval { require Math::Utils } && Math::Utils->can('gcd');
+
 sub main {
-my $gcd = eval { require Math::Utils } && Math::Utils->can('gcd');
-    
+    my($pack) = @_;
+    my $opts = $pack->getoptions;
+     $opts->openout unless $opts->{stdout};
+     $opts->set_rounds;
+     $opts->set_name unless $opts->{name};
+     $opts->total_boards;
+     $opts->oppodata;
+     $opts->eights if $opts->{eight};
+     if ( $opts->{json} ) {
+         $opts->writejson
+     }
+     else {
+         $opts->writeout;
+     }
+     $opts->closeout unless $opts->{stdout};
+}
+
+sub getoptions {
+    my $pack = shift;
     my($file, $force);
-    GetOptions ('-h', \my $help,
+    GetOptions (
+        '-h',   \my $help,
+       '-v',    \my $version,
         '-t=i', \my $teams, 
         '-ew=i', \my $ew_up,
         '-s=s', \my $sessions,
@@ -32,25 +50,17 @@ my $gcd = eval { require Math::Utils } && Math::Utils->can('gcd');
     ) or pod2usage(2);
     
     pod2usage(1) if $help;
+    if ( $version ) {
+        warn "$0: $pack version ". $pack->VERSION ."\n";
+        exit;
+    }
     
     $stdout++ if ($file and $file eq '-');
     $json++ if $eight;
-    unless ($stdout) {
-        my $mode;
-        if ( $json ) {
-            $file = 'config.json' unless $file;
-            $mode = '>';
-        }
-        else {
-            $file = 'TSUserMovements.txt' unless $file;
-            $mode = ($force ? '>' : '>>');
-        }
-        open STDOUT, $mode, $file or die "Can't open $mode $file: $!\n";
-    }
-        
-    my @sessions;
-    @sessions = split /,/, $sessions if defined $sessions;
-    my $total = (sum @sessions) || 0;
+
+    $sessions = [ split /,/, $sessions ]
+        if defined $sessions;
+    my $total = $sessions ? sum @$sessions : 0;
     
     pod2usage ( 
         -message => "Not enough data",
@@ -73,32 +83,82 @@ my $gcd = eval { require Math::Utils } && Math::Utils->can('gcd');
         -exitval => q(NOEXIT),
     ) if @ARGV;
     
-    my $rounds;
-    if( $teams ) {
-        $sitout = $teams % 2;
-        $rounds = $teams;
-        $rounds-- unless $sitout;
+    return bless {
+        file    => $file,
+        force   => $force,
+        teams   => $teams, 
+        ew_up   => $ew_up,
+        total   => $total,
+        boards  => $boards,
+        name    => $name,
+        stdout  => $stdout,   
+        missing => $sitout_boards,
+        miss_ew => $sitout_ew,
+       sitout   => $sitout,
+        json    => $json,
+        eight   =>  $eight,
+        sessions => $sessions,
+    }, $pack;
+} 
+
+sub openout {
+    my $self = shift;
+    my $mode;
+    if ( $self->{json} ) {
+            $self->{file} //= 'config.json';
+            $mode = '>';
     }
     else {
-        $sitout //= (defined $sitout_ew or defined $sitout_boards);
-        $rounds = $total;
-        $rounds++ if $rounds % 2 == 0;
-        $teams = $rounds;
-        $teams++ unless $sitout;
+            $self->{file} //= 'TSUserMovements.txt';
+            $mode = ($self->{force} ? '>' : '>>');
     }
-    
-    unless ($name) {
-      if ( $json ) {
-        $name = 'JSON'
-      }
-      else {
-        require File::Basename;
-        $name = ucfirst (File::Basename::fileparse($0, qr(\.p.*)));
-      }
-    }
+    my $file = $self->{file};
+    open STDOUT, $mode, $file or 
+        die "Can't open $mode $file: $!\n";
+}
         
-    $ew_up = $json ? 2 : $sitout ? 1 : 2 unless $ew_up;
-    $boards = $json ? 0 : int(100/$rounds+0.5) unless $boards;
+sub set_rounds {
+    my $self = shift;
+    if( my $teams = $self->{teams} ) {
+        $self->{sitout} = $teams % 2;
+        $self->{rounds} = $teams;
+        $self->{rounds}-- unless $self->{sitout};
+    }
+    else {
+        $self->{sitout} //=(defined $self->{miss_ew} or
+                            defined $self->{missing} );
+        my $rounds = $self->{total};
+        $rounds++ if $rounds % 2 == 0;
+        $self->{teams} = $self->{rounds} = $rounds;
+        $self->{teams}++ unless $self->{sitout};
+    }
+}
+
+sub set_name {
+    my $self = shift;
+    if ( $self->{json} ) {
+        $self->{name} = 'JSON'
+    }
+    else {
+        require File::Basename;
+        my $basename = File::Basename::fileparse($0, qr(\..*));
+        $self->{name} = ucfirst $basename;  # Flower
+    }
+}
+        
+sub total_boards {
+    my $self = shift;
+    my $json = $self->{json};
+    my $name = $self->{name};
+    my $teams = $self->{teams};
+    my $sitout = $self->{sitout};
+    my $rounds = $self->{rounds};
+    my $ew_up = $self->{ew_up};
+    $ew_up = $self->{ew_up} =  $json ? 2 : $sitout ? 1 : 2
+        unless $ew_up;
+    my $boards = $self->{boards};
+    $boards = $self->{boards} = $json ? 0 : int(100/$rounds+0.5) 
+        unless $boards;
     warn "$name: teams = $teams; sitout = $sitout; rounds = $rounds; ".
         "EW-up = $ew_up; boards = $boards\n";
     if( $gcd ) { 
@@ -106,24 +166,33 @@ my $gcd = eval { require Math::Utils } && Math::Utils->can('gcd');
     }
     
     if ( $sitout ) {
-      unless ( $json ) {
-        unless (defined $sitout_boards or defined $sitout_ew ) {
-            $sitout_boards = 1;     # default to old behaviour
+        unless ( $json ) {
+            unless (defined $self->{missing} or 
+                    defined $self->{miss_ew}) {
+                $self->{missing} = 1;    # default to old behaviour
+            }
+            no warnings qw(uninitialized);
+            warn    "At sitout table".
+                    ": missing boards=$self->{missing}".
+                    "; missing EW=$self->{miss_ew}\n"
         }
-        no warnings qw(uninitialized);
-        warn "At sitout table".
-        ": missing boards=$sitout_boards".
-        "; missing EW=$sitout_ew\n"
-      }
     }
     
-    unshift @sessions, ($rounds - $total);
-    warn "sessions = @sessions\n";
+    my $sessions = $self->{sessions} //= [];
+    unshift @$sessions, ($rounds - $self->{total});
+    warn "sessions = @$sessions\n";
     if ( $boards ) {
         my $total_boards = $rounds * $boards; 
         warn "total boards: $total_boards\n"; 
     }
-    
+}
+
+sub oppodata {
+    my $self = shift;
+    my $ew_up = $self->{ew_up};
+    my $teams = $self->{teams};
+    my $rounds = $self->{rounds};
+    my $sitout = $self->{sitout};
     my @oppodata;
     for my $r (1 .. $rounds) {
         my $rover;
@@ -138,29 +207,45 @@ my $gcd = eval { require Math::Utils } && Math::Utils->can('gcd');
         push @oppo, $rover unless $sitout;
         push @oppodata, \@oppo;
     }
-    
-    if ( $eight ) {
-        for my $round (@oppodata) {
+    $self->{oppodata} = \@oppodata;
+}
+
+sub eights {
+        my $self = shift;
+        my $teams = $self->{teams};
+        for my $round (@{$self->{oppodata}}) {
             die unless $teams == scalar @$round;
             my @repeat = map {$_ + $teams} @$round;
             push @$round, @repeat;
         }
-    }
-    
-    if ( $json ) {
+}
+
+sub writejson {
+        my $self = shift;
         require JSON;
         JSON->import(qw(to_json));
-        my $assignments = to_json(\@oppodata);
+        my $assignments = to_json($self->{oppodata});
         $assignments =~ s/(\],)/$1\n/g;
         print $assignments,"\n";
-    }
-    else {        
-      my $sep = q(, );  # separator between (NS,EW,board-set) triples
-      my $r = 0;
-      for my $s (1 .. $#sessions, 0) {
-        my $session = $sessions[$s];
+}
+
+sub writeout {
+    my $self = shift;
+    my $sessions = $self->{sessions};
+    my $oppodata = $self->{oppodata};
+    my $ew_up   = $self->{ew_up};
+    my $teams   = $self->{teams};
+    my $boards  = $self->{boards};
+    my $sitout  = $self->{sitout};
+    my $missing_boards  = $self->{missing};
+    my $missing_ew      = $self->{miss_ew};
+    my $sep = q(, );  # separator between (NS,EW,board-set) triples
+    my $r = 0;
+    for my $s (1 .. $#{$sessions}, 0) {
+        my $session = $sessions->[$s];
         next unless $session > 0;
-        my $head = sprintf "%s T%d: EW %+d", $name, $teams, $ew_up;
+        my $head = $self->{name};
+        $head .= sprintf " T%d: EW %+d", $teams, $ew_up;
         $head .= ": Session $s" if $s;
         $head .= ": Round";
         $head .= "s" if $session > 1;
@@ -175,14 +260,14 @@ my $gcd = eval { require Math::Utils } && Math::Utils->can('gcd');
         
         for my $ns (1 .. $teams) {
           for my $b (1..$session) {
-            my $ew = $oppodata[$r+$b-1][$ns-1];
+            my $ew = $oppodata->[$r+$b-1][$ns-1];
             print $sep if $b > 1;
     
             my $board_set = $b;
             if ($ew == $ns) {
                 if ($sitout) {
-                    $ew = 0 if $sitout_ew;
-                    $board_set = 0 if $sitout_boards; 
+                    $ew = 0         if $missing_ew;
+                    $board_set = 0  if $missing_boards;
                 }
             }
             print "$ns,$ew,$board_set";
@@ -190,15 +275,15 @@ my $gcd = eval { require Math::Utils } && Math::Utils->can('gcd');
           print "\n";
         }
         $r += $session;
-      }
-      warn "$r rounds: expected $rounds rounds\n" unless $r == $rounds;
     }
-    
-    unless ($stdout) {
-        close STDOUT or die $!;
-        warn "Movement written to $file\n";
-    }
+    warn "$r rounds: expected $self=>{rounds} rounds\n" 
+        unless $r == $self->{rounds};
+}
 
+sub closeout {   
+        my $self = shift;
+        close STDOUT or die $!;
+        warn "Movement written to $self->{file}\n";
 }
 
 1;
